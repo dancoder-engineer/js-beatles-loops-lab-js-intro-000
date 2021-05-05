8aaccfea719a2474e0878a3c25ec0e59b536319f
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 21:34:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function johnLennonFacts(array) {
   
   
 }
+
+
+console.log( expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])))
