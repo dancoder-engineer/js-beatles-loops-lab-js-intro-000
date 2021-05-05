f2616446d1d55949a10a63bb3916461032d79c6e
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 21:28:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(member,instrument) { 
   var sentences = []
   for (let i = 0; i < member,length; i++) {
-    sentences[i] = `${member} plays ${instrument}`
+    sentences[i] = `${member[i]} plays ${instrument[i]}`
   
   }
   return sentences
