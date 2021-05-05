30bdb719cf1d77ac135056cff6bf3dce12601ba8
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 21:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(member,instrument) { 
-  sentences = []
+  var sentences = []
   for (let i = 0; i < member,length; i++) {
     sentences[i] = `${member} plays ${instrument}`
   
